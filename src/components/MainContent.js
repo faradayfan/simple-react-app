41876cc51a47b61ed0931165098f8d73e1ee7370
John Miller <ctrjohnnie@gmail.com>
2018-01-20 22:01:16 -0700

    Added css to the main content page.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,16 +1,15 @@
 import React from 'react'
+import './MainContent.css'
 
 export default class MainContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
         }
     }
-
     render() {
         return (
-            <div>
+            <div className="mainContent">
                 {this.props.pageContent}
             </div>
         )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,7 @@ export default class NavBar extends React.Component {
     }
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand" href="/">{this.props.brandText}</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
